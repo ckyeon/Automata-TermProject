3633f55923b59a8fef0c4925f124e87ad04e68dd
ChoKiyeon <qsky8520@gmail.com>
2021-11-15 21:15:36 +0900

    NFA state table 출력 구현

--- Regular Expression to NFA/Regular Expression to NFA.cpp
+++ Regular Expression to NFA/Regular Expression to NFA.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-#define MAXNODE 100
-#define MAXSTACK 100
+#define MAXNODE 30
+#define MAXSTACK 30
 
 
 struct oneState {
@@ -15,12 +15,6 @@ struct nfaTable {
 	int numOfNode;
 	struct oneState state[MAXNODE];
 } nfaStack[MAXSTACK];
-
-struct oneState a_State_0 = { 1, -1, -1, -1, 0 };
-struct oneState a_State_1 = { -1, -1, -1, -1, 1 };
-
-struct oneState b_State_0 = { -1, 1, -1, -1, 0 };
-struct oneState b_State_1 = { -1, -1, -1, -1, 1 };
 
 
 string read_Regular_Expression();
@@ -55,8 +49,14 @@ string insert_Concatenation_Operator_and_Convert_Into_Postfix_Notation(string in
 {
 	string result;
 
-	for (int i = 0; i < input_String.length(); i++) 
+	int i = 0;
+	while (i < input_String.length())
 	{
+		if (input_String[i] == '^') {
+			i++;
+			continue;
+		}
+
 		if (input_String[i] == '(')
 		{
 			i++;
@@ -76,14 +76,11 @@ string insert_Concatenation_Operator_and_Convert_Into_Postfix_Notation(string in
 			result += '|';
 		}
 
-		if (input_String[i] == '^')
-			continue;
-
-		result += input_String[i];
-
 		if (input_String[i] == 'a' || input_String[i] == 'b')
 		{
+			result += input_String[i];
 			result += '.';
+			i++;
 		}
 	}
 
@@ -94,102 +91,206 @@ void Re_to_NFA(string converted_Input_String)
 {
 	for (int i = 0; i < converted_Input_String.length(); i++) 
 	{
+		if (converted_Input_String[i] == 'a')
+		{
+			struct nfaTable a;
+			
+			a.numOfNode = 2;
+			a.state[0] = { 1, -1, -1, -1, 0 };
+			a.state[1] = { -1, -1, -1, -1, 1 };
+
+			push(a);
+		}
+
+
+		if (converted_Input_String[i] == 'b')
+		{
+			struct nfaTable b;
+
+			b.numOfNode = 2;
+			b.state[0] = { -1, 1, -1, -1, 0 };
+			b.state[1] = { -1, -1, -1, -1, 1 };
+
+			push(b);
+		}
 
 		if (converted_Input_String[i] == '|')
 		{
+			// 스택에서 M2 pop
 			struct nfaTable M2 = pop();
+
+			// 스택에서 M1 pop
 			struct nfaTable M1 = pop();
 
-			struct oneState* copy_M1_State = new struct oneState[M1.numOfNode];
-			struct oneState* copy_M2_State = new struct oneState[M2.numOfNode];
 
+			// M3 nfaTable 정의
+			struct nfaTable M3;
 
-			// M3 nfaTable을 구성할 State 생성
-			
-			// M3의 initial_State
-			// 0 Node(initial_State)에서 1, n(M1) + 1 Node에 lambda edge 삽입
-			struct oneState initial_State = { -1, -1, {1, M1.numOfNode + 1}, 0 };
+			// numOfNode = n(M1) + n(M2) + 2 (initial state, final state 추가로 인한 + 2)
+			M3.numOfNode = M1.numOfNode + M2.numOfNode + 2;
 
-			// M1의 모든 Node 번호에 1씩 더하면서 복사
+			// 0 Node에서 1 과 n(M1) + 1 Node로 lambda edge 삽입
+			M3.state[0] = { -1, -1, { 1, M1.numOfNode + 1 }, 0 };
+
+			// M1의 모든 Node 번호에 1씩 더하면서 M3에 복사
 			for (int i = 0; i < M1.numOfNode; i++)
 			{
 				int a = M1.state[i].a;
 				int b = M1.state[i].b;
+				int e0 = M1.state[i].e[0];
+				int e1 = M1.state[i].e[1];
 
-				copy_M1_State[i] = M1.state[i];
-				copy_M1_State[i].a = (a != -1) ? (a + 1) : -1;
-				copy_M1_State[i].b = (b != -1) ? (b + 1) : -1;
+				M1.state[i].a += (a != -1) ? 1 : 0;
+				M1.state[i].b += (b != -1) ? 1 : 0;
+				M1.state[i].e[0] += (e0 != -1) ? 1 : 0;
+				M1.state[i].e[1] += (e1 != -1) ? 1 : 0;
+
+				// initial state 때문에 i + 1 부터 복사
+				M3.state[i + 1] = M1.state[i];	
 			}
 
-			// M2의 모든 Node 번호에 n(M1) + 1씩 더하면서 복사
+			// n(M1) Node에서 n(M1) + n(M2) + 1 Node로 lambda edge 삽입
+			if (M3.state[M1.numOfNode].e[0] == -1)
+				M3.state[M1.numOfNode].e[0] = M1.numOfNode + M2.numOfNode + 1;
+			else
+				M3.state[M1.numOfNode].e[1] = M1.numOfNode + M2.numOfNode + 1;	
+			// n(M1) Node를 nonfinal로 변경
+			M3.state[M1.numOfNode].output = 0;
+
+			// M2의 모든 Node 번호에 1씩 더하면서 M3에 복사
 			for (int i = 0; i < M2.numOfNode; i++)
 			{
 				int a = M2.state[i].a;
 				int b = M2.state[i].b;
+				int e0 = M2.state[i].e[0];
+				int e1 = M2.state[i].e[1];
 
-				copy_M2_State[i] = M2.state[i];
-				copy_M2_State[i].a = (a != -1) ? (a + M1.numOfNode + 1) : -1;
-				copy_M2_State[i].b = (b != -1) ? (b + M1.numOfNode + 1) : -1;
+				M2.state[i].a += (a != -1) ? (M1.numOfNode + 1) : 0;
+				M2.state[i].b += (b != -1) ? (M1.numOfNode + 1) : 0;
+				M2.state[i].e[0] += (e0 != -1) ? (M1.numOfNode + 1) : 0;
+				M2.state[i].e[1] += (e1 != -1) ? (M1.numOfNode + 1) : 0;
+
+				// M1과 initial state 때문에 i + n(M1) + 1 부터 복사
+				M3.state[i + M1.numOfNode + 1] = M2.state[i]; 
 			}
 
-			// M3의 final_State
-			// n(M1) + n(M2) + 1 Node(final_State)를 final state로 설정
-			struct oneState final_State = { -1, -1, {-1, -1}, 1 };
-
-
-			// M3 nfaTable 정의 및 위에서 정의한 State로 초기화
-			// numOfNode = n(M1) + n(M2) + 2 (initial state, final state 추가로 인한 + 2)
-			struct nfaTable M3 = { M1.numOfNode + M2.numOfNode + 2, initial_State, copy_M1_State[0], copy_M1_State[1], copy_M2_State[0], copy_M2_State[1], final_State };
-
-			// n(M1) Node에서 n(M1) + n(M2) + 1 Node로 lambda edge 삽입
-			M3.state[M1.numOfNode].e[0] = M1.numOfNode + M2.numOfNode + 1;
-			// n(M1) Node를 nonfinal로 변경
-			M3.state[M1.numOfNode].output = 0;
-
 			// n(M1) + n(M2) Node에서 n(M1) + n(M2) + 1 Node로 lambda edge 삽입
-			M3.state[M1.numOfNode + M2.numOfNode].e[0] = M1.numOfNode + M2.numOfNode + 1;
+			if (M3.state[M1.numOfNode + M2.numOfNode].e[0] == -1)
+				M3.state[M1.numOfNode + M2.numOfNode].e[0] = M1.numOfNode + M2.numOfNode + 1;
+			else
+				M3.state[M1.numOfNode + M2.numOfNode].e[1] = M1.numOfNode + M2.numOfNode + 1;
 			// n(M1) + n(M2) Node를 nonfinal로 변경
 			M3.state[M1.numOfNode + M2.numOfNode].output = 0;
+
+			// n(M1) + n(M2) + 1 Node를 final state로 설정
+			M3.state[M1.numOfNode + M2.numOfNode + 1] = { -1, -1, { -1, -1 }, 1 };
 
 
 			// nfaStack에 push
 			push(M3);
+		}
+
+		if (converted_Input_String[i] == '.')
+		{
+			// 스택에서 M2 pop
+			struct nfaTable M2 = pop();
+
+			// 스택에서 M1 pop
+			struct nfaTable M1 = pop();
 
 
-			// 동적할당 해제
-			delete[] copy_M1_State;
-			delete[] copy_M2_State;
+			// M3 nfaTable 정의
+			struct nfaTable M3;
+
+			// numOfNode = n(M1) + n(M2) - 1
+			M3.numOfNode = M1.numOfNode + M2.numOfNode - 1;
+
+			// M1을 M3에 복사
+			for (int i = 0; i < M1.numOfNode; i++)
+			{
+				M3.state[i] = M1.state[i];
+			}
+
+			// n(M1) - 1 Node를 nonfinal로 변경
+			M3.state[M1.numOfNode - 1].output = 0;
+
+			// M2의 모든 Node 번호에 n(M1) - 1씩 더하면서 M3에 복사
+			for (int i = 0; i < M2.numOfNode; i++)
+			{
+				int a = M2.state[i].a;
+				int b = M2.state[i].b;
+				int e0 = M2.state[i].e[0];
+				int e1 = M2.state[i].e[1];
+
+				M2.state[i].a += (a != -1) ? (M1.numOfNode - 1) : 0;
+				M2.state[i].b += (b != -1) ? (M1.numOfNode - 1) : 0;
+				M2.state[i].e[0] += (e0 != -1) ? (M1.numOfNode - 1) : 0;
+				M2.state[i].e[1] += (e1 != -1) ? (M1.numOfNode - 1) : 0;
+
+				// n(M1) - 1 Node가  때문에 i + n(M1) - 1 부터 복사
+				M3.state[i + M1.numOfNode - 1] = M2.state[i];
+			}
+
+			// n(M1) + n(M2) - 2 Node를 final state로 설정
+			M3.state[M1.numOfNode + M2.numOfNode - 2] = { -1, -1, { -1, -1 }, 1 };
+		
+		
+			// nfaStack에 push
+			push(M3);
 		}
 
 		if (converted_Input_String[i] == '*')
 		{
-
-		}
-
-
-		if (i == converted_Input_String.length() - 1)
-			continue;
-
-		if (converted_Input_String[i] == 'a' && converted_Input_String[i + 1] == '.')
-		{
-
-		}
-		else if (converted_Input_String[i] == 'a')
-		{
-			struct nfaTable a_NFA_Table = { 2, a_State_0, a_State_1 };
-			push(a_NFA_Table);
-		}
+			// 스택에서 M1 pop
+			struct nfaTable M1 = pop();
 
 
-		if (converted_Input_String[i] == 'b' && converted_Input_String[i + 1] == '.')
-		{
+			// M3 nfaTable 정의
+			struct nfaTable M3;
 
-		}
-		else if (converted_Input_String[i] == 'b')
-		{
-			struct nfaTable b_NFA_Table = { 2, b_State_0, b_State_1 };
+			// numOfNode = n(M1) + 2 (initial state, final state 추가로 인한 + 2)
+			M3.numOfNode = M1.numOfNode + 2;
 
-			push(b_NFA_Table);
+			// 0 Node에서 1 과 n(M1) + 1 Node로 lambda edge 삽입
+			M3.state[0] = { -1, -1, { 1, M1.numOfNode + 1 }, 0 };
+
+			// M1의 모든 Node 번호에 1씩 더하면서 M3에 복사
+			for (int i = 0; i < M1.numOfNode; i++)
+			{
+				int a = M1.state[i].a;
+				int b = M1.state[i].b;
+				int e0 = M1.state[i].e[0];
+				int e1 = M1.state[i].e[1];
+
+				M1.state[i].a += (a != -1) ? 1 : 0;
+				M1.state[i].b += (b != -1) ? 1 : 0;
+				M1.state[i].e[0] += (e0 != -1) ? 1 : 0;
+				M1.state[i].e[1] += (e1 != -1) ? 1 : 0;
+
+				// initial state 때문에 i + 1 부터 복사
+				M3.state[i + 1] = M1.state[i];
+			}
+
+			// n(M1) Node에서 1 Node로 lambda edge 삽입
+			if (M3.state[M1.numOfNode].e[0] == -1)
+				M3.state[M1.numOfNode].e[0] = 1;
+			else
+				M3.state[M1.numOfNode].e[1] = 1;
+
+			// n(M1) Node에서 n(M1) + 1 Node로 lambda edge 삽입
+			if (M3.state[M1.numOfNode].e[0] == -1)
+				M3.state[M1.numOfNode].e[0] = M1.numOfNode + 1;
+			else
+				M3.state[M1.numOfNode].e[1] = M1.numOfNode + 1;
+			// n(M1) Node를 nonfinal로 변경
+			M3.state[M1.numOfNode].output = 0;
+			
+			// n(M1) + 1 Node를 final state로 설정
+			M3.state[M1.numOfNode + 1] = { -1, -1, { -1, -1 }, 1 };
+
+
+			// nfaStack에 push
+			push(M3);
 		}
 	}
 }
@@ -198,6 +299,18 @@ int main(void)
 {
 	string converted_Input_String = read_Regular_Expression();
 	Re_to_NFA(converted_Input_String);
+
+	cout << "\n변환한 Input_String: " << converted_Input_String << endl;
+	cout << "\nNode의 개수: " << nfaStack[top].numOfNode << endl;
+	cout << "NFA Table" << endl;
+	printf("| state |  a  |  b  | e[0] | e[1] | output |\n");
+	for (int i = 0; i < nfaStack[top].numOfNode; i++)
+	{
+		printf("|   %d   |  %2d |  %2d |  %2d  |  %2d  |   %2d   |\n", i, 
+			nfaStack[top].state[i].a, nfaStack[top].state[i].b,
+			nfaStack[top].state[i].e[0], nfaStack[top].state[i].e[1],
+			nfaStack[top].state[i].output);
+	}
 
 	return 0;
 }
